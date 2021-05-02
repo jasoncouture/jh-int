21edc0af0d60cf34204ab557c77dd021096d695a
Jason Couture <jasonc@alertr.info>
2021-05-02 14:53:16 -0400

    Added controllers to manage repositories

---
+++ BillManager.Api/Controllers/BillController.cs
@@ -0,0 +1,89 @@
+﻿using BillManager.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BillManager.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BillController : ControllerBase
+    {
+        private readonly IBillRepository _billRepository;
+
+        public BillController(IBillRepository billRepository)
+        {
+            _billRepository = billRepository;
+        }
+
+        [HttpGet]
+        [Produces(typeof(IEnumerable<BillModel>))]
+        public async Task<IActionResult> GetBillsAsync(int? count, int? offset, CancellationToken cancellationToken)
+        {
+            List<BillModel> bills = new();
+
+            await foreach (var bill in _billRepository.GetBillsAsync(count, offset).WithCancellation(cancellationToken))
+            {
+                bills.Add(bill);
+            }
+
+            return Ok(bills);
+        }
+
+        [HttpGet("{id}")]
+        [Produces(typeof(BillSummaryModel))]
+        public async Task<IActionResult> GetBillSummary(long id, CancellationToken cancellationToken)
+        {
+            BillSummaryModel billSummary = await _billRepository.GetBillSummaryAsync(id, cancellationToken);
+            if (billSummary == null) return NotFound();
+            return Ok(billSummary);
+        }
+
+        [HttpPost("{billId}/people")]
+        [Produces(typeof(BillSummaryModel))]
+        public async Task<IActionResult> AddPeopleToBill(long billId, IEnumerable<PersonModel> people, CancellationToken cancellationToken)
+        {
+            BillSummaryModel billSummary = await _billRepository.AddPeopleToBillAsync(billId, people.Select(i => i.Id).ToArray(), cancellationToken);
+            if (billSummary == null) return NotFound();
+            return Ok(billSummary);
+        }
+        
+        [HttpDelete("{billId}/people")]
+        [Produces(typeof(BillSummaryModel))]
+        public async Task<IActionResult> RemovePeopleFromBill(long billId, long[] people, CancellationToken cancellationToken)
+        {
+            BillSummaryModel billSummary = await _billRepository.RemovePeopleFromBillAsync(billId, people, cancellationToken);
+            if (billSummary == null) return NotFound();
+            return Ok(billSummary);
+        }
+
+        [HttpPost("{billId}")]
+        [Produces(typeof(BillSummaryModel))]
+        public async Task<IActionResult> UpdateBill(long billId, BillModel billModel, CancellationToken cancellationToken)
+        {
+            BillSummaryModel billSummary = await _billRepository.UpdateBillAsync(billId, billModel, cancellationToken);
+            if (billSummary == null) return NotFound();
+            return Ok(billSummary);
+        }
+        
+        [HttpPost]
+        [Produces(typeof(BillModel))]
+        public async Task<IActionResult> CreateBill(BillModel billModel, CancellationToken cancellationToken)
+        {
+            billModel = await _billRepository.CreateBillAsync(billModel, cancellationToken);
+            return Ok(billModel);
+        }
+
+        [HttpDelete("{billId}")]
+        [Produces(typeof(IEnumerable<ErrorModel>))]
+        public async Task<IActionResult> DeleteBill(long billId, CancellationToken cancellationToken)
+        {
+            IEnumerable<ErrorModel> errors = await _billRepository.DeleteBillAsync(billId, cancellationToken);
+            if (errors.Any())
+                return Ok(errors);
+            return NoContent();
+        }
+    }
+}
---
+++ BillManager.Api/Controllers/PersonController.cs
@@ -0,0 +1,74 @@
+﻿using BillManager.Core;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BillManager.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonRepository _personRepository;
+        public PersonController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        [HttpGet]
+        [Produces(typeof(IEnumerable<PersonModel>))]
+        public async Task<IActionResult> GetAsync(int? count = null, int? offset = null, CancellationToken cancellationToken = default)
+        {
+            List<PersonModel> people = new();
+            await foreach(var person in _personRepository.GetPeople(count, offset).WithCancellation(cancellationToken))
+            {
+                people.Add(person);
+            }
+            return Ok(people);
+        }
+
+        [HttpGet("{id}")]
+        [Produces(typeof(PersonModel))]
+        public async Task<IActionResult> GetAsync(long id, CancellationToken cancellationToken)
+        {
+            PersonModel person = await _personRepository.GetPersonAsync(id, cancellationToken);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [Produces(typeof(PersonModel))]
+        public async Task<IActionResult> PostAsync(PersonModel person, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            person = await _personRepository.CreatePersonAsync(person, cancellationToken);
+            return Ok(person);
+        }
+
+
+        [HttpPost("{id}")]
+        [Produces(typeof(PersonModel))]
+        public async Task<IActionResult> PostAsync([FromRoute] long id, PersonModel person, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            person = await _personRepository.UpdatePersonAsync(id, person, cancellationToken);
+            return Ok(person);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesDefaultResponseType(typeof(IEnumerable<ErrorModel>))]
+        public async Task<IActionResult> DeleteAsync(long id, CancellationToken cancellationToken)
+        {
+            var result = (await _personRepository.DeletePersonAsync(id, cancellationToken)).ToArray();
+            if(result.Any())
+            {
+                return Ok(result);
+            }
+
+            return NoContent();
+        }
+    }
+}
